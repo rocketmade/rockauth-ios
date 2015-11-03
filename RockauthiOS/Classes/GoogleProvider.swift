19f9ca0333fb7188d47a9e4659d6e0baf749dc67
Daniel Gubler <danielcgubler@gmail.com>
2015-11-03 14:01:13 -0700

    Add google plus provider

---
+++ RockauthiOS/Classes/GoogleProvider.swift
@@ -0,0 +1,35 @@
+//
+//  GoogleProvider.swift
+//  Pods
+//
+//  Created by Daniel Gubler on 11/3/15.
+//
+//
+
+import UIKit
+
+public class GoogleProvider: SocialProvider {
+    
+    public static var sharedProvider: SocialProvider! = GoogleProvider()
+    public var token: String?
+    public var name: String = "google_plus"
+    public var secret: String? = nil
+    
+    public init() { }
+    
+    public func login(success success: (user: NSDictionary) -> Void, failure: (error: ErrorType) -> Void) {
+        if let sharedClient = RockauthClient.sharedClient {
+            sharedClient.login(self,
+                success: { (user) -> Void in
+                    success(user: user)
+                }, failure: { (error) -> Void in
+                    failure(error: error)
+            })
+        }
+    }
+    
+    public func logout() {
+        // This is how to logout. Must be done in app for now, since Google is a static framework
+        // GIDSignIn.sharedInstance().signOut()
+    }
+}
